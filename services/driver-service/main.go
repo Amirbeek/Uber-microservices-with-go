d3ee9939a4dd74cf28fb0537d33e931589d06a48
amirbeek <amirbek.shomurodov01@gmail.com>
2025-11-05 21:15:24 +0500

    Driver service and RabitMQ service config tasks are completed

--- services/driver-service/main.go
+++ services/driver-service/main.go
@@ -1,1 +1,58 @@
-package driver_service
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"ride-sharing/services/trip-service/internal/infrastructure/grpc"
+	"ride-sharing/services/trip-service/internal/infrastructure/repository"
+	"ride-sharing/services/trip-service/internal/service"
+	"ride-sharing/shared/env"
+	"syscall"
+
+	grpcserver "google.golang.org/grpc"
+)
+
+var (
+	GrpcAddr = env.GetString("GRPC_ADDR", ":9093")
+)
+
+func main() {
+	inmemRepo := repository.NewInmemRepository()
+	svc := service.NewTripService(inmemRepo)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+		<-sigChan
+		cancel()
+	}()
+
+	lis, err := net.Listen("tcp", GrpcAddr)
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpcserver.NewServer()
+	grpc.NewGRPCHandler(grpcServer, svc)
+
+	log.Println("Starting gRPC server Trip service on port: " + lis.Addr().String())
+
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+			cancel()
+		}
+	}()
+
+	// wait for shutdown signal
+	<-ctx.Done()
+	log.Println("Shutting down gRPC server Trip service")
+	grpcServer.GracefulStop()
+
+}
